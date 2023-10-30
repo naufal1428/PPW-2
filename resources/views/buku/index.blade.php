7d82db3c362a096683b5b87962c6c1479c3e2ab7
naufal1428 <109934685+naufal1428@users.noreply.github.com>
2023-10-30 20:40:50 +0000

    Authentication

--- resources/views/buku/index.blade.php
+++ resources/views/buku/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layout.master')
+{{-- @extends('layout.master')
 
 @section('content')
 
@@ -51,12 +51,179 @@
         </table>
     
         <div class="row">{{ $data_buku->links() }}</div>
-        <div><strong>Jumlah Buku: {{ $jumlah_buku }}</strong></div>
+        <div><strong>Jumlah Buku: {{ $jumlah_buku }}</strong></div> --}}
     
         {{-- <b>Jumlah buku adalah {{ count($data_buku) }}</b> --}}
         {{-- <br>
         <b>Total harga semua buku adalah {{ "Rp ".number_format($totalharga, 2, ',', '.') }}</b> --}}
 
+    {{-- </div>
+    
+@endsection --}}
+
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Data Buku') }}
+        </h2>
+    </x-slot>
+
+    <div class="container">
+        @if (session('pesan'))
+            <div class="alert alert-success">{{ session('pesan') }}</div>
+        @endif
+    
+        <form action="{{ route('buku.search') }}" method="GET" class="search-form">
+            @csrf
+            <input type="text" name="kata" class="form-control" placeholder="Cari...">
+            <button type="submit" class="btn btn-primary">Cari</button>
+        </form>
+    
+        <p><a href="{{ route('buku.create') }}" class="btn btn-success">Tambah Buku</a></p>
+    
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Judul Buku</th>
+                    <th>Penulis</th>
+                    <th>Harga</th>
+                    <th>Tgl. Terbit</th>
+                    <th>Aksi</th>
+                </tr>
+            </thead>
+            <tbody>
+                @forelse($data_buku as $buku)
+                <tr>
+                    <td>{{ $loop->iteration }}</td>
+                    <td>{{ $buku->judul }}</td>
+                    <td>{{ $buku->penulis }}</td>
+                    <td>{{ "Rp " . number_format($buku->harga, 2, ',', '.') }}</td>
+                    <td>{{ $buku->tgl_terbit->format('d/m/Y') }}</td>
+                    <td>
+                        <div class="action-buttons">
+                            <form action="{{ route('buku.destroy', $buku->id) }}" method="post"
+                                onsubmit="return confirm('Yakin mau dihapus?')">
+                                @csrf
+                                
+                                <button type="submit" class="btn-delete">Hapus</button>
+                            </form>
+                            <a href="{{ route('buku.edit', $buku->id) }}" class="btn-edit">Edit</a>
+                        </div>
+                    </td>
+                </tr>                
+                @empty
+                    <tr>
+                        <td colspan="6" class="text-center">Tidak ada data buku</td>
+                    </tr>
+                @endforelse
+            </tbody>
+        </table>
+    
+        <div class="pagination">
+            {{ $data_buku->links() }}
+        </div>
+        <div class="mt-3"><strong>Jumlah Buku: {{ $jumlah_buku }}</strong></div>
     </div>
     
-@endsection
+    <style>
+        .container {
+            max-width: 960px;
+            margin: 0 auto;
+            padding: 20px;
+        }
+    
+        .search-form {
+            margin-bottom: 20px;
+        }
+    
+        .table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-bottom: 20px;
+        }
+    
+        .table th, .table td {
+            padding: 10px;
+            text-align: left;
+            border-bottom: 1px solid #ddd;
+        }
+    
+        .btn {
+            padding: 8px 16px;
+            margin-right: 10px;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+    
+        .btn-primary {
+            background-color: #007bff;
+            color: #fff;
+        }
+    
+        .btn-danger {
+            background-color: #dc3545;
+            color: #fff;
+        }
+    
+        .btn-success {
+            background-color: #28a745;
+            color: #fff;
+        }
+    
+        .pagination {
+            text-align: center;
+        }
+    
+        .pagination ul {
+            display: inline-block;
+            padding: 0;
+            margin: 0;
+        }
+    
+        .pagination li {
+            display: inline;
+            margin-right: 10px;
+        }
+    
+        .pagination li:last-child {
+            margin-right: 0;
+        }
+    
+        .pagination a {
+            text-decoration: none;
+            color: #007bff;
+        }
+    
+        .pagination .active a {
+            font-weight: bold;
+        }
+
+        .action-buttons {
+            display: flex;
+        }
+
+        .btn-delete, .btn-edit {
+            padding: 8px 16px;
+            margin-right: 10px;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+            text-decoration: none;
+            color: #fff;
+            font-weight: bold;
+        }
+
+        .btn-delete {
+            background-color: #dc3545;
+        }
+
+        .btn-edit {
+            background-color: #007bff;
+        }
+
+    </style>
+    
+
+</x-app-layout>
